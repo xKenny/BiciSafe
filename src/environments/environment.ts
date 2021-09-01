3549c207408cccdd2b5a2485d1f2c1acf16e6d3f
david1075 <jmdavidjimenez@gmail.com>
2021-08-31 21:52:08 -0500

    se instalaron dependencias de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD2GIiMIcZ2iOlT3tGhc7rOQ_THptlQNxk",
+    authDomain: "bicisafe-82f32.firebaseapp.com",
+    databaseURL: "https://bicisafe-82f32-default-rtdb.firebaseio.com",
+    projectId: "bicisafe-82f32",
+    storageBucket: "bicisafe-82f32.appspot.com",
+    messagingSenderId: "11290290970",
+    appId: "1:11290290970:web:60888b7be85d966259daa3"
+  }
 };
 
 /*
